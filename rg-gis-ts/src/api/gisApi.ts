cf93a5837fe179139e8926bf99a3a875897dbd7d
Anton Voytov <august-ra@ya.ru>
2025-02-14 00:04:09 +0300

    Added data to the map

--- rg-gis-ts/src/api/gisApi.ts
+++ rg-gis-ts/src/api/gisApi.ts
@@ -29,6 +29,67 @@ export const gisApi = {
     }
   },
 
+  getColor(item: GisRecord): string {
+    if (item.status === "новый")
+      return "#1e98ff"
+    else if (item.status === "в работе")
+      return "#16bb6f"
+    else if (item.status === "отклонено")
+      return "#ff531e"
+
+    return "#1e98ff"
+  },
+
+  getGeoJSON(data: GisInfo) {
+    const objects = []
+    const uniquePoint = {}
+
+    for (const item of data) {
+      // if (есть полигоны) => добавить каждый в массив
+      // if (есть точка) => добавить в массив
+      const key = `${item.x};${item.y}`
+
+      if (uniquePoint[key]) {
+        item.x += uniquePoint[key] * 0.00002
+        ++uniquePoint[key]
+      } else {
+        uniquePoint[key] = 1
+      }
+
+      objects.push({
+        type: "Feature",
+        id:   item._id,
+        geometry: {
+          type:        "Point",
+          coordinates: [item.x, item.y],
+        },
+        properties: {
+          // balloonContent: item.area,
+          // balloonContent: "Аптека",
+          // clusterCaption: "Аптека",
+          // hintContent:    "Аптека",
+          balloonContentHeader: item._id.substring(3),
+          balloonContentBody :  item.address,
+          // iconCaption:    "Аптека",
+          // balloonContentHeader: "Балун метки",
+          // balloonContentBody: "Содержимое <em>балуна</em> метки",
+          // balloonContentFooter: "Подвал",
+          // hintContent: "Хинт метки",
+        },
+        options: {
+          iconColor:             this.getColor(item),
+          preset:                item.status === "в работе" ? "islands#blueCircleDotIcon" : "islands#blueCircleIcon",
+          hideIconOnBalloonOpen: false,
+        },
+      })
+    }
+
+    return {
+      type: "FeatureCollection",
+      features: objects,
+    }
+  },
+
   // writing methods
 
   async addGIS(id: string, data: GisRecord) {
--- rg-gis-ts/src/main.ts
+++ rg-gis-ts/src/main.ts
@@ -5,11 +5,21 @@ import { gisApi } from "./api/gisApi"
 ymaps.ready(init)
 
 async function init() {
-  new ymaps.Map("map", {
-    center: [55.76, 37.64],
-    zoom: 10,
+  const myMap = new ymaps.Map("map", {
+    center:   [55.76, 37.64],
+    zoom:     10,
     controls: [],
   })
 
+  const objectManager = new ymaps.ObjectManager({
+    clusterize:        true,
+    gridSize:          32,
+    clusterIconLayout: "default#pieChart",
+  })
+
   const data = await gisApi.getGIS()
+
+  objectManager.add(gisApi.getGeoJSON(data.filter((item) => selectedAreas.includes(item.area))))
+
+  myMap.geoObjects.add(objectManager)
 }
