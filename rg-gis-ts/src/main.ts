ce80e353ede5ae0b572248b3b8dc954aadf482ee
Anton Voytov <august-ra@ya.ru>
2025-02-14 17:19:02 +0300

    Added controls

--- rg-gis-ts/src/main.ts
+++ rg-gis-ts/src/main.ts
@@ -8,8 +8,26 @@ async function init() {
   const myMap = new ymaps.Map("map", {
     center:   [55.76, 37.64],
     zoom:     10,
-    controls: [],
+    controls: ["fullscreenControl", "typeSelector", "zoomControl"],
   })
+
+  let panoramaManager: ymaps.panorama.Manager | null = null
+
+  const togglePanorama = async (event) => {
+    if (event.get("type") === "select") {
+      if (!panoramaManager)
+        panoramaManager = await myMap.getPanoramaManager()
+
+      panoramaManager.enableLookup()
+    } else {
+      panoramaManager.disableLookup()
+    }
+  }
+
+  const panoramaButton = new ymaps.control.Button("Панорама")
+  panoramaButton.events.add(["select", "deselect"], togglePanorama)
+
+  myMap.controls.add(panoramaButton, { float: "right" })
 
   const objectManager = new ymaps.ObjectManager({
     clusterize:        true,
